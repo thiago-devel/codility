a6bbd8fc95b3c173bbf2a093d2d85bbeb9107a87
Thiago Santana <thiagosantana@ac-tsantana-MAC.local>
2018-08-08 20:09:10 -0300

    Still I need to fixed

--- codility_python/src/Lesson2_Arrays_part2.py
+++ codility_python/src/Lesson2_Arrays_part2.py
@@ -1,4 +1,3 @@
-# Need to be fixed before submit
 def oddOcurrencyInArray(A):
     result = 0
     
@@ -24,18 +23,25 @@ arrayExpected = 7
 result = oddOcurrencyInArray(array)
 print 'In the provided array {' + str(array) + '} the odd item is {' + str(result) + '} and the expected was {' + str(arrayExpected) + '}'
 
-array = [44, 3, 44, 102, 9, 102, 9, 2]
-arrayExpected = 3
+array = [44, 3, 44, 3, 9, 102, 102]
+arrayExpected = 9
 result = oddOcurrencyInArray(array)
 print 'In the provided array {' + str(array) + '} the odd item is {' + str(result) + '} and the expected was {' + str(arrayExpected) + '}'
 
-array = [201]
+array = []
+i = 0
+while (i < 200):
+    next = i + 1
+    array.insert(i, i)
+    if (i < 200):
+        array.insert(next, i)
+    i = next
 arrayExpected = 201
 result = oddOcurrencyInArray(array)
 print 'In the provided array {' + str(array) + '} the odd item is {' + str(result) + '} and the expected was {' + str(arrayExpected) + '}'
-
+'''
 array = [33, 33, 2.001, 8, 7, 7, 8]
 arrayExpected = 2.001
 result = oddOcurrencyInArray(array)
 print 'In the provided array {' + str(array) + '} the odd item is {' + str(result) + '} and the expected was {' + str(arrayExpected) + '}'
-
+'''
